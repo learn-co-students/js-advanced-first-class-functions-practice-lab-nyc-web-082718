c208b996712dedab9bba418f288f098a45f36898
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-10 16:48:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(drivers, hometown) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === hometown) { console.log(driver.name) }
+  })
+}
+
+function driversByRevenue(drivers) {
+  const drivers_copy = drivers.slice()
+  return drivers_copy.sort(function(a, b) {
+    return a.revenue > b.revenue
+  })
+}
+
+function driversByName(drivers) {
+  const drivers_copy = drivers.slice()
+  return drivers_copy.sort(function(a, b) {
+    return a.name > b.name
+  })
+}
+
+function totalRevenue(drivers) {
+  const acc = (accumulator, driver) => accumulator + driver.revenue
+  return drivers.reduce(acc, 0)
+}
+
+function averageRevenue(drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
